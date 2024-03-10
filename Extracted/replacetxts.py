05e34ecc06aa8451cb5f9e9c48235ecea4fa629b
Newbie511 <al5yal511@hotmail.com>
2024-03-10 21:18:32 +0300

    quests

--- Extracted/replacetxts.py
+++ Extracted/replacetxts.py
@@ -2,8 +2,8 @@ import os
 import shutil
 
 # Source and destination directories
-source_dir = r"C:\arabic 4.0\Extracted\World\Quests"
-dest_dir = r"C:\Users\al5ya\OneDrive\Documents\GitHub\Texts_AR\Extracted\World\Quests"
+source_dir = r"C:\arabic 4.0\Extracted\World\Ask"
+dest_dir = r"C:\Users\al5ya\OneDrive\Documents\GitHub\Texts_AR\Extracted\World\Ask"
 
 # Walk through the source directory
 for root, _, files in os.walk(source_dir):
